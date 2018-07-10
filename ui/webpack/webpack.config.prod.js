f8241d2b1676668d7f3d476081d5180c1047bff7
syuyuusyu <syuyuusyu@gmail.com>
2018-07-10 16:25:28 +0800

    打包优化

--- ui/webpack/webpack.config.prod.js
+++ ui/webpack/webpack.config.prod.js
@@ -5,44 +5,45 @@ const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const commonConfig = require('./webpack.config.common');
 
 const prodConfig = {
-  mode: 'production',
-  optimization: {
-    minimizer: [
-      new UglifyJSPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: false // set to true if you want JS source maps
-      }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist/*.*'])
-  ],
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        antd: {
-          chunks: "initial",
-          test: "antd",
-          name: "antd",
-          enforce: true
-        },
-        mobx: {
-          chunks: "initial",
-          test: "mobx",
-          name: "mobx",
-          enforce: true
-        },
-        react: {
-          chunks: "initial",
-          test: "react",
-          name: "react",
-          enforce: true
+    devtool: 'source-map',
+    mode: 'production',
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: false // set to true if you want JS source maps
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist/*.*'])
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                antd: {
+                    chunks: "initial",
+                    test: "antd",
+                    name: "antd",
+                    enforce: true
+                },
+                mobx: {
+                    chunks: "initial",
+                    test: "mobx",
+                    name: "mobx",
+                    enforce: true
+                },
+                react: {
+                    chunks: "initial",
+                    test: "react",
+                    name: "react",
+                    enforce: true
+                }
+            }
         }
-      }
     }
-  }
 };
 
 module.exports = merge(commonConfig, prodConfig);
